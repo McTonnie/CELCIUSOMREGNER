d7e66c6162be398045a76b51ad8b4a3678b654df
Oliver Tobias Jensen <Olivertobias@live.dk>
2021-01-25 10:42:16 +0100

    Add project files.

---
+++ CELCIUSOMREGNER/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CELCIUSOMREGNER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Intast grader i Celcius");
+            double Input = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Reamur " + Input * 0.8);
+            Console.WriteLine("Fahrenheit " + (Input * 1.8 + 32));
+            Console.ReadKey(); 
+        }
+    }
+}
